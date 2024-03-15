37571d2be48d736cd497b26d292570ccd27154be
Walter Celi <antonini2903@hotmail.com>
2024-03-15 10:18:25 -0500

    Base layout drawers will hide on URL change.

--- uae-tareas-simulacion/Components/BaseLayout.js
+++ uae-tareas-simulacion/Components/BaseLayout.js
@@ -1,15 +1,42 @@
 import Head from "next/head"
-import { Container, Header, Content, Sidebar, CustomProvider, Navbar, Nav, Tag } from "rsuite"
+import { Container, Header, Content, Sidebar, CustomProvider, Navbar, Nav, Tag, Drawer } from "rsuite"
 import { SIMULADORES } from '../I18n/es/simulators'
 import Link from "next/link"
 import { useRouter } from "next/router"
 
 import 'katex/dist/katex.min.css'
+import { useEffect, useReducer, useState } from "react"
+import { Button } from "rsuite"
+import _ from "lodash"
 
 
 
 export const BaseLayout = ({title, children, rightContent, ...props}) => {
-  const {pathname} = useRouter()
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const [drawerContent, setDrawerContent] = useState()
+  const [placement, setPlacement] = useState()
+
+  const handleDrawer = (content, placement) => {
+    setDrawerOpen(Boolean(content))
+    setDrawerContent(content)
+    setPlacement(content ? placement : null)
+  }
+
+  const SidebarMenu = () => <Nav vertical appearance="subtle">
+    {SIMULADORES.map(s => {
+      const active = pathname === s.url
+
+      return <Nav.Item key={s.url} href={s.url} as={Link} active={active}
+        style={{ flexWrap: "wrap" }}
+      >
+        <Tag color={active ? "blue" : "grey"}>{s.abbreviation}</Tag>
+        <p className="w-100">{s.title}</p>
+      </Nav.Item>
+    })}
+  </Nav>
+
+  const {pathname, query} = useRouter()
+  useEffect(() => handleDrawer(), [pathname, query])
 
   return <>
     <CustomProvider theme="dark">
@@ -20,7 +47,11 @@ export const BaseLayout = ({title, children, rightContent, ...props}) => {
         <Header className="border-bottom">
           <Nav className="d-flex w-100">
             <Nav.Item as={Link} href="/"><i className="bi bi-house"/></Nav.Item>
-            <Nav.Item><i className="bi bi-list"/></Nav.Item>
+            <Nav.Item className="d-xs-block d-lg-none"
+              onClick={() => handleDrawer(<SidebarMenu />, "left")}
+            >
+              <i className="bi bi-list"/>
+            </Nav.Item>
             <Nav.Item style={{ 
               flexBasis: 1, 
               flexShrink: 0, 
@@ -30,38 +61,42 @@ export const BaseLayout = ({title, children, rightContent, ...props}) => {
             }}>
               <span className="mx-auto">{title}</span>
             </Nav.Item>
-            <Nav.Item><i className="bi bi-question"/></Nav.Item>
+            <Nav.Item className="d-xs-block d-xl-none"
+              onClick={() => handleDrawer(rightContent, "right")}
+            >
+              <i className="bi bi-question"/>
+            </Nav.Item>
             <Nav.Item><i className="bi bi-sliders"/></Nav.Item>
           </Nav>
         </Header>
         <Content style={{ backgroundColor: "black",  }}>
           <Container>
             <Sidebar className="d-none d-lg-block d-xl-block d-xxl-block pe-3" style={{}}>
-              <Nav vertical>
-                {SIMULADORES.map(s => {
-                  const active = pathname === s.url
-                  
-                  return <Nav.Item key={s.url} href={s.url} as={Link} active={active}
-                    style={{ flexWrap: "wrap" }}
-                  >
-                    <Tag color={active ? "blue" : "grey"}>{s.abbreviation}</Tag>
-                    <p className="w-100">{s.title}</p>
-                  </Nav.Item>
-                })}
-              </Nav>
+              <SidebarMenu />
             </Sidebar>
             <Content>
               <div className="mt-3 "/>
-              <div style={{ backgroundColor: "#222", minHeight: "calc( 100vh - 60px - 3em )" }}>
+              <div className="overflow-scroll" style={{ backgroundColor: "#222", height: "calc( 100vh - 60px - 3em )" }}>
                 {children}
               </div>
             </Content>
-            <Sidebar className="d-none d-xl-block d-xxl-block ps-3 pt-2">
+            <Sidebar className="d-none d-xl-block d-xxl-block ps-3 pt-2 overflow-scroll">
               {rightContent}
             </Sidebar>
           </Container>
         </Content>
       </Container>
+      <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)} placement={placement} 
+        size="xs"
+        onExited={() => {
+          setPlacement(null)
+          setDrawerContent(null)
+        }}
+      >
+        <Drawer.Body>
+          {drawerContent}
+        </Drawer.Body>
+      </Drawer>
     </CustomProvider>
   </>
 }
