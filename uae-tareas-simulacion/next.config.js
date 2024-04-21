0766b86791b1b6d078332d6be1520f90cbd32d2d
Walter Celi <antonini2903@hotmail.com>
2024-04-21 17:12:41 -0500

    Project META
    - Dependencies
    - Language config
    - TypeScript config

--- uae-tareas-simulacion/next.config.js
+++ uae-tareas-simulacion/next.config.js
@@ -3,6 +3,13 @@ const withLess = require('next-with-less');
 const nextConfig = withLess({
   reactStrictMode: true,
   swcMinify: true,
-});
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+}); 
 
 module.exports = nextConfig
